3eadb1de21597c5ec4f7f06f6038b7ae7506851a
Manuel Deutsch <manuel.deutsch27@gmail.com>
2020-11-23 09:31:56 +0100

    Projekt fertig

--- Program.cs
+++ Program.cs
@@ -12,6 +12,24 @@ namespace Lernzeit
             zeiterfassungen.Add(new Zeiterfassung(60, "Programmierung"));
             zeiterfassungen.Add(new Zeiterfassung(123, "Mathematik"));
             zeiterfassungen.Add(new Zeiterfassung(25, "Deutsch"));
+
+            Console.WriteLine("Für folgende Fächer wurde gelernt:");
+
+            foreach (var Zeiterfassung in zeiterfassungen)
+            {
+                Console.WriteLine($"- {Zeiterfassung.Fach}");
+            }
+
+            Console.WriteLine();
+
+            int sumLernzeit = 0;
+
+            foreach (var Zeiterfassung in zeiterfassungen)
+            {
+                sumLernzeit = sumLernzeit + Zeiterfassung.Lernzeit;
+            }
+
+            Console.WriteLine($"Lernzeit: {sumLernzeit} min");
         }
     }
 }
